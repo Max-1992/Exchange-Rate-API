86b5ec078eadd208b9331b6d748a75963322e15e
Maximiliano Echevarria <maechevarria@proveedor.teco.com.ar>
2021-10-25 09:57:00 -0300

    feat: the infrastructure layer was created with the Fixer, http and operationDate modules

---
+++ src/infraestructure/operationDateService/operationDate.js
@@ -0,0 +1,15 @@
+class OperationDate {
+    
+    #dateModule
+
+    constructor (dateModule) {
+        this.#dateModule = dateModule
+    }
+
+    createDateWithFormat(format) {
+        const date = this.#dateModule().format(format)
+        return date
+    }
+}
+
+module.exports = OperationDate
