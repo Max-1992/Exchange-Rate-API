1aaa71a36ab7368b653366f8147a0859d96bd6a5
Maximiliano Echevarria <maechevarria@proveedor.teco.com.ar>
2021-10-25 22:32:18 -0300

    refactor: driver error handling removed

--- src/api/controller.js
+++ src/api/controller.js
@@ -4,14 +4,8 @@ class RatesController {
   }
 
   async getAll (request, reply) {
-    try {
-      const rates = await this.ratesService.getAllRates()
-      return reply.response(rates).code(200)
-    } catch (error) {
-      console.error(error)
-      const message = 'The service is not available at this time, please try again later.'
-      reply.response(message).code(500)
-    }
+    const rates = await this.ratesService.getAllRates()
+    return reply.response(rates).code(200)
   }
 }
 
