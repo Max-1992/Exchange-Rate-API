513be8a17678e5aba956d3342fa80b759827db51
Maximiliano Echevarria <maechevarria@proveedor.teco.com.ar>
2021-10-25 23:10:33 -0300

    refactor: the business rules module was adjusted to the standard of standard.js

--- src/domain/business/rates.business.js
+++ src/domain/business/rates.business.js
@@ -1,59 +1,51 @@
 const pairs = require('./pairs/pairs')
 
 class RatesBusinessRules {
+  constructor (fxService, quote, pricing) {
+    this.fxService = fxService
+    this.quote = quote
+    this.pricing = pricing
+  }
 
-    #fxService
-    #quote
-    #pricing
+  async getFxRates () {
+    const fxRates = this.fxService.getHistoricalRates()
+    return fxRates
+  }
 
-    constructor(fxService, quote, pricing) {
-        this.#fxService = fxService
-        this.#quote = quote
-        this.#pricing = pricing
+  quoteCurrencyPairs (fxRates) {
+    const currencyPairs = []
+
+    for (const pair in pairs) {
+      const courrency = pairs[pair]
+      courrency.pair = pair
+
+      if (courrency.base === fxRates.base) {
+        courrency.amount = fxRates.rates[courrency.quote]
+        currencyPairs.push(courrency)
+        continue
+      }
+
+      const baseCurrencyValue = fxRates.rates[courrency.base]
+      const quoteCurrencyValue = fxRates.rates[courrency.quote]
+
+      const currencyQuote = this.quote.calculateQuoteCurrencies(baseCurrencyValue, quoteCurrencyValue)
+      courrency.amount = currencyQuote
+
+      currencyPairs.push(courrency)
     }
 
-    async getFxRates () {
-        const fxRates = this.#fxService.getHistoricalRates()
-        return fxRates
-    }
+    return currencyPairs
+  }
 
-    quoteCurrencyPairs (fxRates) {
+  addMarkUp (pair) {
+    const markUpAmount = this.pricing.markUp(pair.markUp, pair.amount)
+    const markUpApplied = this.pricing.applyNegatively(pair.amount, markUpAmount)
 
-        const currencyPairs = []
+    pair.feeAmount = markUpAmount
+    pair.rateMarkUpApplied = markUpApplied
 
-        for (const pair in pairs) {
-
-            const courrency = pairs[pair]
-            courrency.pair = pair
-
-            if (courrency.base === fxRates.base) {
-                courrency.amount = fxRates.rates[courrency.quote]
-                currencyPairs.push(courrency)
-                continue;
-            }
-
-            const baseCurrencyValue = fxRates.rates[courrency.base]
-            const quoteCurrencyValue =  fxRates.rates[courrency.quote]
-
-            const currencyQuote = this.#quote.calculateQuoteCurrencies(baseCurrencyValue, quoteCurrencyValue)
-            
-            courrency.amount = currencyQuote
-
-            currencyPairs.push(courrency)
-        }
-
-        return currencyPairs
-    }
-
-    addMarkUp (pair) {
-        const markUpAmount = this.#pricing.markUp(pair.markUp, pair.amount)
-        const markUpApplied = this.#pricing.applyNegatively(pair.amount, markUpAmount)
-
-        pair.feeAmount = markUpAmount
-        pair.rateMarkUpApplied = markUpApplied
-
-        return pair
-    }
+    return pair
+  }
 }
 
 module.exports = RatesBusinessRules
